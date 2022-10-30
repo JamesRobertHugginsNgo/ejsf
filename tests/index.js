439161ef8c2896f435c50e1054da930790b4ca0f
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2022-10-30 19:38:50 -0400

    Update tests

--- tests/index.js
+++ tests/index.js
@@ -7,7 +7,7 @@ const Ejsf = require('../index');
 const data = {
 	title: 'TITLE & TITLE',
 	subTitle: 'SUB TITLE',
-	body: '<TESTING>'
+	body: '<p>Proin sodales dui at quam vehicula, sit amet laoreet dolor interdum. Duis suscipit urna at ligula euismod dapibus. Phasellus quis viverra velit. Nam diam ante, pulvinar vitae ultricies nec, molestie a massa. Maecenas gravida, eros vel finibus vehicula, nibh mauris dictum nunc, eu luctus purus augue ac neque. Mauris eleifend eros quis viverra pretium. Pellentesque non diam tempor, sollicitudin augue id, pellentesque dui.</p>'
 };
 
 // Create EJS function
--- tests/results/template.js
+++ tests/results/template.js
@@ -1,8 +1,4 @@
-module.exports = (_data) => { const _esc = (value) => value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, "&#x27;").replace(/\//g, '&#x2F;'); const _includes = { 'template-2': (_data) => { const subTitle = _data.subTitle; let _out = ''; _out += `<div>`; _out += ( subTitle ); _out += `</div>
-`; return _out; } }; const include = (path, data) => _includes[path](data); const title = _data.title; const body = _data.body; const subTitle = _data.subTitle; let _out = ''; _out += `<!DOCTYPE html>
-<html lang="en">
-
-<head>
+module.exports = (_data) => { const _includes = { 'head': (_data) => { const _esc = (value) => value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, "&#x27;").replace(/\//g, '&#x2F;'); const title = _data.title; let _out = ''; _out += `<head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,15 +6,29 @@ module.exports = (_data) => { const _esc = (value) => value.replace(/&/g, '&amp;
 		`; _out += _esc( title ); _out += `
 	</title>
 </head>
+`; return _out; }, 'header': (_data) => { const _esc = (value) => value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, "&#x27;").replace(/\//g, '&#x2F;'); const title = _data.title; let _out = ''; _out += `<header>
+	<h1>`; _out += _esc( title ); _out += `</h1>
+</header>
+`; return _out; }, 'footer': (_data) => { let _out = ''; _out += `<footer>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit velit sed purus venenatis ultrices. Proin
+		condimentum varius lobortis. Donec mollis elit nec ante interdum posuere et eu turpis. Donec tincidunt eros ipsum,
+		quis accumsan dui eleifend a. Nulla ut tincidunt elit, nec tincidunt libero. Suspendisse quis turpis quis velit
+		faucibus blandit sit amet ut lectus. Sed maximus leo ex, in scelerisque leo fringilla ac. Mauris faucibus nec nisi
+		et scelerisque.</p>
+</footer>
+`; return _out; } }; const include = (path, data) => _includes[path](data); const title = _data.title; const body = _data.body; let _out = ''; _out += `<!DOCTYPE html>
+<html lang="en">
+
+`; _out += ( include('head', { title }) ); _out += `
 
 <body>
-	<h1>`; _out += ( title ); _out += `</h1>
+	`; _out += ( include('header', { title}) ); _out += `
 
 	`;  if (body) {  _out += `
-		<p>`; _out += _esc( body ); _out += `</p>
+		`; _out += ( body ); _out += `
 	`;  }  _out += `
 
-	`; _out += ( include('template-2', { subTitle }) ); _out += `
+	`; _out += ( include('footer') ); _out += `
 </body>
 
 </html>
